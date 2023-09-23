78596301453d1bf7f5b57a95025f2620fbd02080
ITR090 <raghad.4309@gmail.com>
2023-09-24 00:07:18 +0300

    new changes

--- src/UI/header/Header.js
+++ src/UI/header/Header.js
@@ -5,7 +5,7 @@ const Header = (props) => {
         // page header   
         // row gx-4 gx-lg-5 justify-content-center
         // col-md-10 col-lg-8 col-xl-7
-        <header className="masthead" style={{backgroundImage :"url(http://localhost:3000/assets/img/home-bg.jpg)"}}>
+        <header className="masthead" style={{backgroundImage :`url(${props.imageURL})`}}>
             <div className="container position-relative px-4 px-lg-5">
                 <div className="justify-content-center">
                     <div className="">
--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -1,12 +1,12 @@
 
-import React, { useEffect, useState, useContext,useCallback } from 'react'
+import React, { useEffect, useState, useContext, useCallback } from 'react'
 import { useParams } from 'react-router-dom'
 import CommentForm from '../Comments/CommentForm'
 import { CommentContext } from '../../store/commentStoreContext';
 import Comments from '../Comments/Comments';
 import Header from '../../UI/header/Header';
-import {AppContext} from '../../store/appStoreContext'
-import {AuthContext} from '../../store/loginStoreContext'
+import { AppContext } from '../../store/appStoreContext'
+import { AuthContext } from '../../store/loginStoreContext'
 import { storage } from '../../firebaseConfig';
 import { ref, getDownloadURL } from "firebase/storage";
 
@@ -23,24 +23,24 @@ const Post = () => {
     const Post_Id = useParams()
 
     const [post, setPost] = useState()
-    const [imageURL,setImageURL] = useState(null)
+    const [imageURL, setImageURL] = useState(null)
     //const [comments, setComments] = useState()
-    
+
     const getPost = useCallback(async () => {
         //console.log("get post")
         const response = await fetch(`https://react-blog-app-45e74-default-rtdb.europe-west1.firebasedatabase.app/Posts/${Post_Id.id}.json`);
         const responseData = await response.json()
         //console.log("log : "+post)
         setPost(responseData)
-        getDownloadURL(ref(storage,responseData.imagePath)).
-        then(url=>{
-            setImageURL(url)
-        })
+        getDownloadURL(ref(storage, responseData.imagePath)).
+            then(url => {
+                setImageURL(url)
+            })
         ctxComment.getAllComments()
         //setComments()
-    },[])
+    }, [])
 
-    
+
 
     useEffect(() => {
         getPost();
@@ -48,9 +48,9 @@ const Post = () => {
 
     const onClickLikePost = (post_id) => {
 
-        const updatedPost ={
+        const updatedPost = {
             ...post,
-            Likes : post.Likes +1
+            Likes: post.Likes + 1
         }
         setPost(updatedPost)
         // setPost(PrevPostState => {
@@ -59,27 +59,27 @@ const Post = () => {
         //         Likes : PrevPostState.Likes + 1
         //     }
         // })
-       ctxPost.likePost(updatedPost,post_id)
+        ctxPost.likePost(updatedPost, post_id)
     }
 
-    const onAddComment = (comment,db)=>{
-        
+    const onAddComment = (comment, db) => {
+
         //let updatedComments = comments.concat(comment)
         //console.log(updatedComments)
         //setComments(updatedComments)
-        ctxComment.AddComment(comment,db)
+        ctxComment.AddComment(comment, db)
     }
 
-    
+
 
     return (
         <>
             {post !== undefined ?
                 <>
-                    <Header>
+                    <Header imageURL={imageURL}>
                         <div className="post-heading">
                             <h1>{post.title}</h1>
-                           
+
                             <span className="meta">
                                 Posted by
                                 <> {post.userName} </>
@@ -97,24 +97,23 @@ const Post = () => {
                     <article className="mb-4">
                         <div className="container px-4 px-lg-5">
                             <div className="row justify-content-center mt-5 mb-5">
-                                <div className="">
+                                {post.imagePath && <img src={imageURL} className='img-fluid img-thumbnail' />}
+                                <div className="mt-5">
                                     <p className='text-break'>{post.content}</p>
-                                    {post.imagePath && <img src={imageURL}  className='img-fluid img-thumbnail' />}
-                                    {Object.keys(ctxAuth.user) !== null && 
-                                    <p className='mb-5 mt-5'>
-                                        Did you like this post click here to add it to your liked posts
-                                        <i className="p-2 fa-solid fa-heart fa-beat" style={{ color: "#ca1c1c" }} onClick={() => onClickLikePost(Post_Id)}></i>
-                                    </p>}
-
-
+                                    {/* {post.imagePath && <img src={imageURL}  className='img-fluid img-thumbnail' />} */}
+                                    {Object.keys(ctxAuth.user) !== null &&
+                                        <p className='mb-5 mt-5'>
+                                            Did you like this post click here to add it to your liked posts
+                                            <i className="p-2 fa-solid fa-heart fa-beat" style={{ color: "#ca1c1c" }} onClick={() => onClickLikePost(Post_Id)}></i>
+                                        </p>}
                                 </div>
                             </div>
                             <CommentForm onaddCommentCallback={onAddComment} post={post} />
                             <br />
                             <h5>Recent Comments:</h5>
-                            {ctxComment.comments && 
-                            <Comments postComments={ctxComment.comments.filter(postComment => postComment.post_id == post.id)}
-                                post_id={post.id} />}
+                            {ctxComment.comments &&
+                                <Comments postComments={ctxComment.comments.filter(postComment => postComment.post_id == post.id)}
+                                    post_id={post.id} />}
                         </div>
                     </article>
 
